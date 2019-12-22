72bdf741fcf59dbe21f84e177e3c0219ce3f5b86
Josh Justice <josh.justice@accesscfa.com>
2019-12-22 08:12:55 -0500

    Specify onSend should be called

--- tests/integration/components/new-message-form-test.js
+++ tests/integration/components/new-message-form-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, fillIn, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
 
 module('Integration | Component | new-message-form', function(hooks) {
   setupRenderingTest(hooks);
@@ -15,5 +16,19 @@ module('Integration | Component | new-message-form', function(hooks) {
     await click('[data-test-send-message-button]');
 
     assert.dom('[data-test-message-text]').hasValue('');
+  });
+
+  test('it calls the onSend argument with the message', async function(assert) {
+    const message = 'Hello World';
+
+    const handleSend = sinon.spy();
+    this.set('handleSend', handleSend);
+
+    await render(hbs`<NewMessageForm @onSend={{handleSend}} />`);
+
+    await fillIn('[data-test-message-text]', message);
+    await click('[data-test-send-message-button]');
+
+    assert.ok(handleSend.calledWith(message));
   });
 });
