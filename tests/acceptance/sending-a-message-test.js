52a9fd3f1f9d2adc9b2e3ad90a4606625eb8b39f
Josh Justice <josh.justice@accesscfa.com>
2019-12-22 07:09:35 -0500

    Specify sending a message

--- tests/acceptance/sending-a-message-test.js
+++ tests/acceptance/sending-a-message-test.js
@@ -1,13 +1,20 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, fillIn, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Acceptance | sending a message', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
-  test('visiting /sending-a-message', async function(assert) {
-    await visit('/sending-a-message');
+  test('it shows the message in the list', async function(assert) {
+    const message = 'Hello World';
+    await visit('/');
 
-    assert.equal(currentURL(), '/sending-a-message');
+    await fillIn('[data-test-message-text]', message);
+    await click('[data-test-send-message-button]');
+
+    assert.dom('[data-test-message-text]').hasValue('');
+    assert.dom('[data-test-message-list]').hasText(message);
   });
 });
