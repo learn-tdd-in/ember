87e9e255b9ac6521185a3fe2979c63319eef9506
Josh Justice <josh.justice@accesscfa.com>
2019-12-22 07:45:35 -0500

    Specify clearing the message

---
+++ tests/integration/components/new-message-form-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, fillIn, click } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | new-message-form', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it clears the message', async function(assert) {
+    const message = 'Hello World';
+
+    await render(hbs`<NewMessageForm />`);
+
+    await fillIn('[data-test-message-text]', message);
+    await click('[data-test-send-message-button]');
+
+    assert.dom('[data-test-message-text]').hasValue('');
+  });
+});
